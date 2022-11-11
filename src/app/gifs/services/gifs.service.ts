aa1369cf137e96a3df84cb6dbe23f826995345d0
Antonio Galisteo <agalisteo@vitamina.cl>
2022-11-11 10:43:19 -0500

    API call refactor

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
@@ -8,6 +8,7 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 export class GifsService {
 
   private apiKey: string = 'PUT_YOUR_API_KEY_HEAR';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
   
   public resultados : Gif[] = [];
@@ -34,7 +35,12 @@ export class GifsService {
 
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=P0WM88uKHCU9CeKqFDgvzMJ3ZOF3KCpE&q=${ query }&limit=10`)
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, {params})
     .subscribe(resp => {
       console.log(resp.data);
       this.resultados = resp.data;
